51753cdde760a37cff64174f7663094d280209d2
Hammad-Kinnect2 <hammad.dogar@kinnect2.com>
2015-10-30 11:44:27 +0500

    Initial Commit

---
+++ Kinnect2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kinnect2
+//
+//  Created by Hammad Bin Hamid on 10/30/15.
+//  Copyright (c) 2015 Hammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kinnect2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kinnect2
+//
+//  Created by Hammad Bin Hamid on 10/30/15.
+//  Copyright (c) 2015 Hammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
